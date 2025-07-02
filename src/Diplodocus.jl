e2e3c68b492fd8f0d0be986a49fb14f72c7f4583
Christopher Everett <cn.everett@outlook.com>
2025-07-02 11:50:20 +0100

    updates to imports and exports

--- src/Diplodocus.jl
+++ src/Diplodocus.jl
@@ -1,3 +1,25 @@
 module Diplodocus
 
+    # Transport Exports
+    export LoadMatrices, BigMatrices, FluxMatrices
+    export PhaseSpaceStruct, MomentumStruct, SpaceStruct, TimeStruct, OutputStruct
+    export BinaryStruct, EmiStruct, ForceType
+    export Cylindrical, Spherical, Cartesian, Axi
+    export SyncRadReact
+    export BuildBigMatrices, BuildFluxMatrices
+    export Initial_Constant, Initial_MaxwellJuttner, Initial_PowerLaw
+    export Solve, EulerStruct
+
+    # Plot Exports 
+    export DiplodocusDark, DiplodocusLight
+    export MomentumDistributionPlot
+
+    # RecursiveArrayTools
+    export ArrayPartition
+
+    using DiplodocusCollisions
+    using DiplodocusTransport
+    using DiplodocusPlots
+    using RecursiveArrayTools
+
 end
