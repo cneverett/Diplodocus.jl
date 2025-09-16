9b20f3355561409db6bfe923bab75b3990c7bb30
Christopher Everett <cn.everett@outlook.com>
2025-09-16 14:44:23 +0100

    + SSC files

---
+++ examples/Blazar/BlazarSynchrotronCollisionMatrix.jl
@@ -0,0 +1,50 @@
+    using Diplodocus
+
+# ========= Generate Collision Matrices ========== #
+
+    # Particle definitions
+    name1::String = "Ele";
+    name2::String = "Ele";
+    name3::String = "Pho";
+    type::String = "Sync";
+
+    # Momentum space grids
+    p_low_Ele = 0e0
+    p_up_Ele = 7e0
+    p_grid_Ele = "l"
+    p_num_Ele = 28
+    u_grid_Ele = "u"
+    u_num_Ele = 9
+    h_grid_Ele = "u"
+    h_num_Ele = 1
+
+    p_low_Pho = -15e0
+    p_up_Pho = 7e0
+    p_grid_Pho = "l"
+    p_num_Pho = 88
+    u_grid_Pho = "u"
+    u_num_Pho = 9
+    h_grid_Pho = "u"
+    h_num_Pho = 1
+
+    # Define the External parameters. For synchrotron Ext[1] = B field in Tesla 
+    Ext::Vector{Float64} = [1e-4,];
+
+    # number of points to sample
+    numLoss = 128p_num_Pho*u_num_Pho*h_num_Pho
+    numGain = 512p_num_Pho*u_num_Pho*h_num_Pho
+    numThreads = 15
+    # scale factor range
+    scale = 0.0:0.1:0.0
+
+    # file location and setup
+    fileLocation = pwd()*"\\examples\\Data";
+    (Setup,fileName) = UserEmissionParameters()
+
+    # run the integration
+    EmissionInteractionIntegration(Setup)
+
+# ===== Check Accuracy of  Collision Matrices ===== #
+
+    Output = EmissionFileLoad_Matrix(fileLocation,fileName); 
+    InteractiveEmissionGainLossPlot(Output)
---
+++ examples/Blazar/BlazarTransport.jl
@@ -0,0 +1,120 @@
+using Diplodocus
+
+# ==== Define domains of time and space ====== #
+
+    B = 1e-4
+
+    t_up::Float64 = log10(SIToCodeUnitsTime(1e6))
+    t_low::Float64 = log10(SIToCodeUnitsTime(1e0))
+    t_num::Int64 = 600
+    t_grid::String = "l"
+
+    #t_up::Float64 = SIToCodeUnitsTime(1e2) # -12e0 seconds * (σT*c)
+    #t_low::Float64 = SIToCodeUnitsTime(0e0) #-20e0 seconds * (σT*c)
+    #t_num::Int64 = 2000
+    #t_grid::String = "u"
+
+    time = TimeStruct(t_up,t_low,t_num,t_grid)
+
+    space_coords = Cylindrical(0.0,0.5*2/3,0.0)# x = r, y = phi, z = z
+
+    x_up::Float64 = 1.0
+    x_low::Float64 = 0f0
+    x_grid::String = "u"
+    x_num::Int64 = 1 
+
+    y_up::Float64 = 2.0*pi
+    y_low::Float64 = 0.0
+    y_grid::String = "u"
+    y_num::Int64 = 1
+
+    z_up::Float64 = 1.0
+    z_low::Float64 = 0.0
+    z_grid::String = "u"
+    z_num::Int64 = 1
+
+    space = SpaceStruct(space_coords,x_up,x_low,x_grid,x_num,y_up,y_low,y_grid,y_num,z_up,z_low,z_grid,z_num)
+
+# ==== Define particles and their momentum space domains ====== #
+
+    name_list::Vector{String} = ["Ele","Pho"];
+
+    momentum_coords = Spherical() # px = p, py = u, pz = phi
+
+    px_up_list::Vector{Float64} = [7.0,7.0];
+    px_low_list::Vector{Float64} = [-3.0,-15.0];
+    px_grid_list::Vector{String} = ["l","l"];
+    px_num_list::Vector{Int64} = [80,88];
+
+    py_up_list::Vector{Float64} = [1.0,1.0];
+    py_low_list::Vector{Float64} = [-1.0,-1.0];
+    py_grid_list::Vector{String} = ["u","u"];
+    py_num_list::Vector{Int64} = [9,9];
+
+    pz_up_list::Vector{Float64} = [2.0pi,2.0pi];
+    pz_low_list::Vector{Float64} = [0.0,0.0];
+    pz_grid_list::Vector{String} = ["u","u"];
+    pz_num_list::Vector{Int64} = [1,1]
+    momentum = MomentumStruct(momentum_coords,px_up_list,px_low_list,px_grid_list,px_num_list,py_up_list,py_low_list,py_grid_list,py_num_list,pz_up_list,pz_low_list,pz_grid_list,pz_num_list,"upwind");
+
+# ==== Define Interactions  ====== #
+
+    Binary_list::Vector{BinaryStruct} = [BinaryStruct("Ele","Pho","Ele","Pho"),];
+    Emi_list::Vector{EmiStruct} = [EmiStruct("Ele","Ele","Pho","Sync",[B],Ani()),];
+    Forces::Vector{ForceType} = [CoordinateForce(),SyncRadReact(Ani(),B),];
+
+    # build phase space
+    PhaseSpace = PhaseSpaceStruct(name_list,time,space,momentum,Binary_list,Emi_list,Forces);
+
+# location of DataDirectory where Interaction Matrices are stored
+
+    DataDirectory = pwd()*"\\examples\\Data"
+
+# Load interaction matrices
+
+    BigM = BuildBigMatrices(PhaseSpace,DataDirectory;loading_check=true);
+    FluxM = BuildFluxMatrices(PhaseSpace);
+
+    GC.gc()
+
+# Set initial conditions
+
+    Initial = Initialise_Initial_Condition(PhaseSpace);
+    Initial_UnBoostedPowerLaw!(Initial,PhaseSpace,"Ele",pmin=1e3,pmax=1e5,Gamma=2.0,index=2.0,num_Init=1e6);
+    #Initial_PowerLaw!(Initial,PhaseSpace,"Ele",pmin=1e3,pmax=1e6,umin=-1.0,umax=1.0,hmin=0.0,hmax=2.0,index=2.0,num_Init=1e6);
+    #Initial_BlackBody!(Initial,PhaseSpace,"Pho",T=3.0,umin=-1.0,umax=1.0,hmin=0.0,hmax=2.0,num_Init=1e15);
+
+    N = Diplodocus.DiplodocusTransport.FourFlow(Initial[1:80*9],80,9,1,PhaseSpace.Grids.pxr_list[1],PhaseSpace.Grids.pyr_list[1],PhaseSpace.Grids.pzr_list[1],1.0)
+    U = Diplodocus.DiplodocusTransport.HydroFourVelocity(N)
+
+# ===== Run the Solver ================== #
+
+    scheme = EulerStruct(Initial,PhaseSpace,BigM,FluxM,false)
+    fileName = "Blazar_Ani_10div3.jld2";
+    fileLocation = pwd()*"\\examples\\Data";
+
+    #VSCodeServer.Profile.clear()
+    #@profview sol = Solve(Initial,scheme;save_steps=10,progress=true,fileName=fileName,fileLocation=fileLocation);
+    @time sol = Solve(Initial,scheme;save_steps=10,progress=true,fileName=fileName,fileLocation=fileLocation);
+
+# ===== Load and Plot Results ================== # 
+
+    fileLocation = pwd()*"\\examples\\Data";
+    fileName = "Blazar_Iso.jld2";
+    (PS_Iso, sol_Iso) = SolutionFileLoad(fileLocation,fileName);
+    fileName = "Blazar_Ani_0.jld2";
+    (PS_Ani0, sol_Ani0) = SolutionFileLoad(fileLocation,fileName);
+    fileName = "Blazar_Ani_5.jld2";
+    (PS_Ani5, sol_Ani5) = SolutionFileLoad(fileLocation,fileName);
+    fileName = "Blazar_Ani_5div3.jld2";
+    (PS_Ani5div3, sol_Ani5div3) = SolutionFileLoad(fileLocation,fileName);
+    fileName = "Blazar_Ani_10div3.jld2";
+    (PS_Ani10div3, sol_Ani10div3) = SolutionFileLoad(fileLocation,fileName);
+
+    SSCBFieldDark = Diplodocus.DiplodocusPlots.BFieldObserverPlot([sol_Ani0,sol_Ani5div3,sol_Ani10div3,sol_Ani5,sol_Iso],[PS_Ani0,PS_Ani5div3,PS_Ani10div3,PS_Ani5,PS_Iso],52,0.1,plot_limits=(-17.0,7.0,-0.5,8.5),title=nothing,TimeUnits=CodeToSIUnitsTime,theme=DiplodocusDark())
+    SSCBFieldLight = Diplodocus.DiplodocusPlots.BFieldObserverPlot([sol_Ani0,sol_Ani5div3,sol_Ani10div3,sol_Ani5,sol_Iso],[PS_Ani0,PS_Ani5div3,PS_Ani10div3,PS_Ani5,PS_Iso],52,0.1,plot_limits=(-17.0,7.0,-0.5,8.5),title=nothing,TimeUnits=CodeToSIUnitsTime,theme=DiplodocusLight())
+
+    Diplodocus.DiplodocusPlots.save("SSCBFieldDark.svg",SSCBFieldDark)
+    Diplodocus.DiplodocusPlots.save("SSCBFieldDark.pdf",SSCBFieldDark)
+    Diplodocus.DiplodocusPlots.save("SSCBFieldLight.svg",SSCBFieldLight)
+    Diplodocus.DiplodocusPlots.save("SSCBFieldLight.pdf",SSCBFieldLight)
