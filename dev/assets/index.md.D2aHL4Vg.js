f858341be173fa070a3d89b2e58867b7c780ca50
Documenter.jl <documenter@juliadocs.github.io>
2025-12-04 11:27:19 +0000

    build based on b944c6e

---
+++ dev/assets/index.md.D2aHL4Vg.js
@@ -0,0 +1,33 @@
+import{_ as a,c as n,o as e,aA as t}from"./chunks/framework.Dz-_QPPc.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"Diplodocus.jl","text":"Particle transport for Astrophysics","tagline":"A Julia framework for evaluating transport of particle distributions through phase space for mesoscopic modelling of astrophysical sources","image":{"src":"/test1.gif","alt":"Diplodocus","width":480,"height":480},"actions":[{"theme":"alt","text":"View on Github","link":"https://github.com/cneverett/Diplodocus"}]},"features":[{"title":"Get Started","details":"Overview of the Diplodocus framework, installation guide and the codes conventions","link":"/Get Started/overview"},{"title":"Tutorials","details":"Worked examples of how to use Diplodocus","link":"/Tutorials/tutorials"}]},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":null}'),i={name:"index.md"};function p(o,s,r,l,c,d){return e(),n("div",null,[...s[0]||(s[0]=[t(`<h2 id="Citing-Diplodocus.jl" tabindex="-1">Citing Diplodocus.jl <a class="header-anchor" href="#Citing-Diplodocus.jl" aria-label="Permalink to &quot;Citing Diplodocus.jl {#Citing-Diplodocus.jl}&quot;">​</a></h2><p>If you use Diplodocus.jl, any of its components or are just feeling kind, please cite one or all of the main Diplodocus series papers:</p><blockquote><p>C. N. Everett and G. Cotter, DIPLODOCUS I: Framework for the evaluation of relativistic transport equations with continuous forcing and discrete particle interactions (2025), <a href="https://doi.org/10.48550/arXiv.2508.13296" target="_blank" rel="noreferrer">https://doi.org/10.48550/arXiv.2508.13296</a></p></blockquote><blockquote><p>C. N. Everett, M. Klinger-Plaisier and G. Cotter, DIPLODOCUS II: Implementation of Transport Equations and Test Cases Relevant to Micro-Scale Physics of Jetted Astrophysical Sources (2025), <a href="https://doi.org/10.48550/arXiv.2510.12505" target="_blank" rel="noreferrer">https://doi.org/10.48550/arXiv.2510.12505</a></p></blockquote><p>(Paper III coming soon)</p><details class="details custom-block"><summary>Show BibTeX</summary><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>@article{EverettCotter_2025,</span></span>
+<span class="line"><span>       author = {{Everett}, Christopher N. and {Cotter}, Garret},</span></span>
+<span class="line"><span>        title = &quot;{DIPLODOCUS I: Framework for the evaluation of relativistic transport equations with continuous forcing and discrete particle interactions}&quot;,</span></span>
+<span class="line"><span>      journal = {arXiv e-prints},</span></span>
+<span class="line"><span>     keywords = {High Energy Astrophysical Phenomena},</span></span>
+<span class="line"><span>         year = 2025,</span></span>
+<span class="line"><span>        month = aug,</span></span>
+<span class="line"><span>          eid = {arXiv:2508.13296},</span></span>
+<span class="line"><span>        pages = {arXiv:2508.13296},</span></span>
+<span class="line"><span>          doi = {10.48550/arXiv.2508.13296},</span></span>
+<span class="line"><span>archivePrefix = {arXiv},</span></span>
+<span class="line"><span>       eprint = {2508.13296},</span></span>
+<span class="line"><span> primaryClass = {astro-ph.HE},</span></span>
+<span class="line"><span>       adsurl = {https://ui.adsabs.harvard.edu/abs/2025arXiv250813296E},</span></span>
+<span class="line"><span>      adsnote = {Provided by the SAO/NASA Astrophysics Data System}</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>@article{EverettEtAl_2025,</span></span>
+<span class="line"><span>       author = {{Everett}, Christopher N. and {Klinger-Plaisier}, Marc and {Cotter}, Garret},</span></span>
+<span class="line"><span>        title = &quot;{DIPLODOCUS II: Implementation of transport equations and test cases relevant to micro-scale physics of jetted astrophysical sources}&quot;,</span></span>
+<span class="line"><span>      journal = {arXiv e-prints},</span></span>
+<span class="line"><span>     keywords = {High Energy Astrophysical Phenomena},</span></span>
+<span class="line"><span>         year = 2025,</span></span>
+<span class="line"><span>        month = oct,</span></span>
+<span class="line"><span>          eid = {arXiv:2510.12505},</span></span>
+<span class="line"><span>        pages = {arXiv:2510.12505},</span></span>
+<span class="line"><span>          doi = {10.48550/arXiv.2510.12505},</span></span>
+<span class="line"><span>archivePrefix = {arXiv},</span></span>
+<span class="line"><span>       eprint = {2510.12505},</span></span>
+<span class="line"><span> primaryClass = {astro-ph.HE},</span></span>
+<span class="line"><span>       adsurl = {https://ui.adsabs.harvard.edu/abs/2025arXiv251012505E},</span></span>
+<span class="line"><span>      adsnote = {Provided by the SAO/NASA Astrophysics Data System}</span></span>
+<span class="line"><span>}</span></span></code></pre></div></details>`,6)])])}const v=a(i,[["render",p]]);export{h as __pageData,v as default};
