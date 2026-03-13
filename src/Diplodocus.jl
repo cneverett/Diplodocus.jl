e53d726930d713d5273d60b30ea1e2ec532a5be1
cneverett <cn.everett@outlook.com>
2026-03-13 22:38:32 +0000

    update export list for v0.2 compat of DiplodocusCollisions, DiplodocusTransport and DiplodocusPlots

--- src/Diplodocus.jl
+++ src/Diplodocus.jl
@@ -5,38 +5,36 @@ module Diplodocus
     using DiplodocusPlots
 
     # Collision Exports
-    export UserBinaryParameters
-    export BinaryInteractionIntegration
-    export BinaryFileLoad_Matrix, BinaryFileLoad_All, DoesConserve
-    export UserEmissionParameters
-    export EmissionInteractionIntegration
-    export EmissionFileLoad_Matrix, EmissionFileLoad_All
+    export BinaryInteractionIntegration, BinaryFileLoad_All, DoesConserve, BinaryFileLoad_Matrix
+    export EmissionInteractionIntegration, EmissionFileLoad_Matrix, EmissionFileLoad_All
+    export UserBinaryParameters, UserEmissionParameters
 
     # Transport Exports
     export LoadMatrices, BigMatrices, FluxMatrices
-    export PhaseSpaceStruct, MomentumStruct, SpaceStruct, TimeStruct, OutputStruct
+    export PhaseSpaceStruct, MomentumStruct, SpaceStruct, TimeStruct, OutputStruct, CharacteristicStruct, GridsStruct, ElectroMagneticFieldStruct
     export BinaryStruct, EmiStruct, ForceType
-    export Cylindrical, Spherical, Cartesian, Ani, Axi, Iso
-    export CoordinateForce, SyncRadReact
-    export BuildBigMatrices, BuildFluxMatrices
+    export BackendType
+    export CoordinateType, Cylindrical, Spherical, Cartesian
+    export ModeType, Ani, Axi, Iso
+    export BoundaryType, Periodic, Open, Closed, Reflective
+    export CoordinateForce, SyncRadReact, GradBInvZDecay
+    export BuildBinaryMatrices, BuildEmissionMatrices, BuildFluxMatrices
     export Initialise_Initial_Condition, Location_Species_To_StateVector, Initial_Constant!, Initial_MaxwellJuttner!, Initial_PowerLaw!, Initial_BoostedPowerLaw!, Initial_BlackBody!
-    export Solve, EulerStruct
+    export Initialise_Injection_Condition, Injection_Constant!, Injection_MaxwellJuttner!, Injection_PowerLaw!, Injection_BoostedPowerLaw!, Injection_BlackBody!
+    export ElectroMagneticField_Constant, ElectroMagneticField_InvZDecay
+    export CollisionDomain
+    export Solve, ForwardEulerStruct, ForwardSymplecticEulerStruct
     export SolutionFileLoad
+    export MaxwellJuttner_Distribution, BlackBody_Distribution, Wien_Distribution
+    export CUDABackend, CPUBackend
 
     # Plot Exports 
     export DiplodocusDark, DiplodocusLight
     export Static, Animated, Interactive
-    export MomentumDistributionPlot, MomentumAndPolarAngleDistributionPlot, MomentumComboAnimation
-    export InteractiveBinaryGainLossPlot, MomentumAndAzimuthalAngleDistributionPlot, AzimuthalAngleDistributionPlot
-    export TimeScalePlot
-    export FracNumberDensityPlot, NumberDensityPlot
-    export FracEnergyDensityPlot, EnergyDensityPlot
-    export IsThermalPlot, IsIsotropicPlot, IsThermalAndIsotropicPlot
-    export InteractiveEmissionGainLossPlot
-    export CodeToCodeUnitsTime, CodeToSIUnitsTime, SIToCodeUnitsTime, SyncToCodeUnitsTime, CodeToSyncUnitsTime
-    export ObserverFluxPlot
-
-    # RecursiveArrayTools
-    export ArrayPartition
+    export InteractiveBinaryGainLossPlot, InteractiveEmissionGainLossPlot
+    export MomentumDistributionPlot0D, MomentumAndPolarAngleDistributionPlot0D
+    export NumberDensityPlot0D
+    export EnergyDensityPlot0D
+    export IsThermalAndIsotropicPlot0D
 
 end
