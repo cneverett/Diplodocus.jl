3e1caa36b233270850d4788f2cc71296daaf8441
cneverett <cn.everett@outlook.com>
2026-03-13 19:48:43 +0000

    attempt makedocs from dev branch

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ bib = CitationBibliography(
 makedocs(;
     modules = [Diplodocus,DiplodocusCollisions],
     plugins = [bib],
-    repo = Remotes.GitHub("cneverett","Diplodocus.jl"),
+    repo = Remotes.GitHub("cneverett","Diplodocus.jl#dev"),
     authors = "Christopher Everett",
     sitename = "Diplodocus.jl",
     format = DocumenterVitepress.MarkdownVitepress(
