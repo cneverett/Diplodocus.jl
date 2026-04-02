8f649a448fb68ba64020a848db0ce055434c7e37
Documenter.jl <documenter@juliadocs.github.io>
2026-04-02 14:45:39 +0000

    build based on 6879022

---
+++ dev/assets/index.md.D-dIfjpC.js
@@ -0,0 +1,33 @@
+import{_ as a,o as n,c as e,aA as t}from"./chunks/framework.iCvwNLJz.js";const h=JSON.parse('{"title":"What is Diplodocus.jl?","description":"","frontmatter":{"layout":"home","hero":{"name":"Diplodocus.jl","text":"","tagline":"Particle Transport and Emissions for Astrophysics","image":{"src":"/test1.gif","alt":"Diplodocus","width":480,"height":480},"actions":[{"theme":"brand","text":"Get Started","link":"/Get Started/overview"},{"theme":"alt","text":"Tutorials","link":"/Tutorials/overview"},{"theme":"alt","text":"View on Github","link":"https://github.com/cneverett/Diplodocus"}]}},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":null}'),i={name:"index.md"};function p(o,s,l,r,c,d){return n(),e("div",null,[...s[0]||(s[0]=[t(`<h1 id="What-is-Diplodocus.jl?" tabindex="-1">What is Diplodocus.jl? <a class="header-anchor" href="#What-is-Diplodocus.jl?" aria-label="Permalink to &quot;What is Diplodocus.jl? {#What-is-Diplodocus.jl?}&quot;">​</a></h1><p>Diplodocus.jl is a Julia package for modelling of high energy astrophysical sources such as jets from Active Galactic Nuclei, Gamma-Ray Bursts, and X-Ray Binaries.</p><p>It acts as a bridge between macro-scale simulations of spatial structure and dynamics (e.g. GRMHD) and micro-scale modelling of particle interactions an emissive processes (e.g. one-/multi-zone approaches); capable of including both regimes! Diplodocus.jl solves the phase-space transport of particle distribution functions, including: arbitrary forces and asymmetric interactions on a curved spacetime background with options to add complex electromagnetic field structures.</p><p>By evolving particles in space, momentum, and time, Diplodocus.jl captures of all the these multi-scale effects simultaneously and self-consistently, allowing the generation of multi-wavelength emission spectra and imaging through which the true physics of a wide range of astrophysical objects can be studied.</p><h2 id="Citing-Diplodocus.jl" tabindex="-1">Citing Diplodocus.jl <a class="header-anchor" href="#Citing-Diplodocus.jl" aria-label="Permalink to &quot;Citing Diplodocus.jl {#Citing-Diplodocus.jl}&quot;">​</a></h2><p>If you use Diplodocus.jl, any of its components or are just feeling kind, please cite one or all of the main Diplodocus series papers (Paper III coming soon):</p><blockquote><p>C. N. Everett and G. Cotter, DIPLODOCUS I: Framework for the evaluation of relativistic transport equations with continuous forcing and discrete particle interactions (2026), The Open Journal of Astrophysics, 9, <a href="https://doi.org/10.33232/001c.155822" target="_blank" rel="noreferrer">https://doi.org/10.33232/001c.155822</a></p></blockquote><blockquote><p>C. N. Everett, M. Klinger-Plaisier and G. Cotter, DIPLODOCUS II: Implementation of Transport Equations and Test Cases Relevant to Micro-Scale Physics of Jetted Astrophysical Sources (2026), The Open Journal of Astrophysics, 9, <a href="https://doi.org/10.33232/001c.157823" target="_blank" rel="noreferrer">https://doi.org/10.33232/001c.157823</a></p></blockquote><details class="details custom-block"><summary>Show BibTeX</summary><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>@ARTICLE{DiplodocusI,</span></span>
+<span class="line"><span>       author = {{Everett}, Christopher N. and {Cotter}, Garret},</span></span>
+<span class="line"><span>        title = &quot;{DIPLODOCUS I: Framework for the evaluation of relativistic transport equations with continuous forcing and discrete particle interactions}&quot;,</span></span>
+<span class="line"><span>      journal = {The Open Journal of Astrophysics},</span></span>
+<span class="line"><span>     keywords = {High Energy Astrophysical Phenomena},</span></span>
+<span class="line"><span>         year = 2026,</span></span>
+<span class="line"><span>        month = jan,</span></span>
+<span class="line"><span>       volume = {9},</span></span>
+<span class="line"><span>        pages = {55822},</span></span>
+<span class="line"><span>          doi = {10.33232/001c.155822},</span></span>
+<span class="line"><span>archivePrefix = {arXiv},</span></span>
+<span class="line"><span>       eprint = {2508.13296},</span></span>
+<span class="line"><span> primaryClass = {astro-ph.HE},</span></span>
+<span class="line"><span>       adsurl = {https://ui.adsabs.harvard.edu/abs/2026OJAp....955822E},</span></span>
+<span class="line"><span>      adsnote = {Provided by the SAO/NASA Astrophysics Data System}</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>@ARTICLE{DiplodocusII,</span></span>
+<span class="line"><span>       author = {{Everett}, Christopher N. and {Klinger-Plaisier}, Marc and {Cotter}, Garret},</span></span>
+<span class="line"><span>        title = &quot;{DIPLODOCUS II: Implementation of transport equations and test cases relevant to micro-scale physics of jetted astrophysical sources}&quot;,</span></span>
+<span class="line"><span>      journal = {The Open Journal of Astrophysics},</span></span>
+<span class="line"><span>     keywords = {High Energy Astrophysical Phenomena},</span></span>
+<span class="line"><span>         year = 2026,</span></span>
+<span class="line"><span>        month = feb,</span></span>
+<span class="line"><span>       volume = {9},</span></span>
+<span class="line"><span>        pages = {57823},</span></span>
+<span class="line"><span>          doi = {10.33232/001c.157823},</span></span>
+<span class="line"><span>archivePrefix = {arXiv},</span></span>
+<span class="line"><span>       eprint = {2510.12505},</span></span>
+<span class="line"><span> primaryClass = {astro-ph.HE},</span></span>
+<span class="line"><span>       adsurl = {https://ui.adsabs.harvard.edu/abs/2026OJAp....957823E},</span></span>
+<span class="line"><span>      adsnote = {Provided by the SAO/NASA Astrophysics Data System}</span></span>
+<span class="line"><span>}</span></span></code></pre></div></details>`,9)])])}const m=a(i,[["render",p]]);export{h as __pageData,m as default};
